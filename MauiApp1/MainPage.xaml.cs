f2aa555b95354423daacda6a57a805c43fda59c3
Phil108 <>
2024-11-04 16:56:17 +0100

    * add sample code to reproduce icon font rendering error

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        var fontImageSource = new FontImageSource();
+        fontImageSource.FontFamily = "iconfont";
+        fontImageSource.Glyph = Convert.ToChar(Convert.ToUInt32($"0x{0xe00b.ToString("X4").ToUpper()}", 16)).ToString();
+        fontImageSource.Color = Colors.Fuchsia;
+        fontImageSource.Size = 100;
+
+        FontImage.Source = fontImageSource;
+    }
+}
